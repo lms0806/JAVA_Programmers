42663d9f140eb98a841860ff3b9a0120ce0cd0a4
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-09 00:09:25 +0900

    Create 체육복.java

---
+++ level1/체육복.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int count = 0;
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                }
+            }
+        }
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if(lost[i] == (reserve[j]-1)){
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                }
+                if(lost[i] == (reserve[j]+1)){
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                }
+            }
+        }
+        
+        for(int i = 0; i < lost.length; i++){
+            if(lost[i] != -1){
+                count++;
+            }
+        }
+        
+        answer = n - count;
+        return answer;
+    }
+}
