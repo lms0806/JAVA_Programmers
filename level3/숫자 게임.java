8a82b7b2166ef1534629d43018f1cd49daf2569f
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-11 00:26:36 +0900

    Create 숫자 게임.java

---
+++ level3/숫자 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        int i = 0, j = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        while(j != B.length){
+            if(A[i] < B[j]){
+                answer++;
+                i++;
+                j++;
+            }
+            else if(A[i] >= B[j]){
+                j++;
+            }
+        }
+        return answer;
+    }
+}
