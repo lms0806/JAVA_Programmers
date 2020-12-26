aa729f1e603752f46b9e6d0c09ce51694ca14130
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-26 17:09:59 +0900

    Create 두 개 뽑아서 더하기.java

---
+++ level1/두 개 뽑아서 더하기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int size = numbers.length * (numbers.length-1) / 2;
+        
+        int[] num = new int[size];
+        
+        int k = 0;
+        for(int i = 0; i < numbers.length-1; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                num[k] = numbers[i] + numbers[j];
+                k++;
+            }
+        }
+        
+        Arrays.sort(num);
+        
+        int nums = num[0];
+        int count = 1;
+        for(int i = 1; i < num.length; i++){
+            if(nums != num[i]){
+                nums = num[i];
+                count++;
+            }
+        }
+        
+        nums = num[0];
+        int[] answer = new int[count];
+        count = 1;
+        answer[0] = num[0];
+        for(int i = 1; i < num.length; i++){
+            if(nums != num[i]){
+                nums = num[i];
+                answer[count] = nums;
+                count++;
+            }
+        }
+        return answer;
+    }
+}
