e0a7aab176b514c15065733dc9a3b6bcdc1431be
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-04 17:31:33 +0900

    Create 주식가격.java

---
+++ level2/주식가격.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i < prices.length; i++){
+            for(int j = i+1; j < prices.length; j++){
+                if(prices[i] <= prices[j]){
+                    answer[i]++;
+                }
+                else {
+                    answer[i]++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
