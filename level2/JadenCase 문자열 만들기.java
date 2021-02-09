28f02f437dd6da40cfdc54f32d75df47327b3b19
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 23:19:50 +0900

    Update JadenCase 문자열 만들기.java

--- level2/JadenCase 문자열 만들기.java
+++ level2/JadenCase 문자열 만들기.java
@@ -5,16 +5,12 @@ class Solution {
         String[] str = s.split("");
         
         for(int i = 0; i < str.length; i++){
-            if(i == 0){
-                str[i] = str[i].toUpperCase();
-            }
-            else if(str[i-1].equals(" ")){
-                str[i] = str[i].toUpperCase();
+            if(i == 0 || str[i-1].equals(" ")){
+                answer += str[i].toUpperCase();
             }
             else{
-                str[i] = str[i].toLowerCase();
+                answer += str[i];
             }
-            answer+=str[i];
         }
         return answer;
     }
