4762cd89d618d9a48f77d855da3979deae219105
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 22:53:05 +0900

    Update 이진 변환 반복하기.java

--- level2/이진 변환 반복하기.java
+++ level2/이진 변환 반복하기.java
@@ -3,17 +3,18 @@ class Solution {
         int[] answer = new int[2];
         
         while(s.length() != 1){
-            String[] str = s.split("");
-            s = "";
-            for(int i = 0; i < str.length; i++){
-                if(str[i].equals("1")){
-                    s += str[i];
+            String str = "";
+            for(int i = 0; i < s.length(); i++){
+                char ch = s.charAt(i);
+                if(ch == '1'){
+                    str += ch;
                 }
             }
             answer[0]++;
-            answer[1] += str.length - s.length();
-            s = Integer.toBinaryString(s.length());
+            answer[1] += s.length() - str.length();
+            s = Integer.toBinaryString(str.length());
         }
+        
         return answer;
     }
 }
