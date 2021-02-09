a055c8d7f8c11f267437f0e6d216c1a47bb4d262
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 23:28:30 +0900

    Update 문자열 내 p와 y의 개수.java

--- level1/문자열 내 p와 y의 개수.java
+++ level1/문자열 내 p와 y의 개수.java
@@ -1,14 +1,13 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        String[] a = s.split("");
         int count = 0, count1 = 0;
         
-        for(int i = 0; i < a.length; i++){
-            if(a[i].equals("p") || a[i].equals("P")){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'P' || s.charAt(i) == 'p'){
                 count++;
             }
-            if(a[i].equals("Y") || a[i].equals("y")){
+            if(s.charAt(i) == 'Y' || s.charAt(i) == 'y'){
                 count1++;
             }
         }
