05611cfb07accedf0bcf9fc47f5f432e99bd10fa
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 23:24:27 +0900

    Update 2016년.java

--- level1/2016년.java
+++ level1/2016년.java
@@ -19,30 +19,10 @@ class Solution {
               b+=31;
           }
       }
-      b-=1;
-      switch(b%7){
-          case 0:
-              answer = "FRI";
-              break;
-          case 1:
-              answer = "SAT";
-              break;
-          case 2:
-              answer = "SUN";
-              break;
-          case 3:
-              answer = "MON";
-              break;
-          case 4:
-              answer = "TUE";
-              break;
-          case 5:
-              answer = "WED";
-              break;
-          case 6:
-              answer = "THU";
-              break;
-      }
+      
+      String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+      answer = day[(b-1)%7];
+      
       return answer;
   }
 }
