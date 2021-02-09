13186d23bf42ffc4dbc3999b46455e05f921c964
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 22:49:05 +0900

    Update 3진법 뒤집기.java

--- level1/3진법 뒤집기.java
+++ level1/3진법 뒤집기.java
@@ -2,14 +2,15 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         String str = "";
+        
         while(n != 0){
             str+=n%3;
             n /= 3;
         }
-        String[] strs = str.split("");
+        
         int nums = 1;
-        for(int i = strs.length-1; i >= 0; i--){
-            answer += Integer.parseInt(strs[i]) * nums;
+        for(int i = str.length()-1; i >= 0; i--){
+            answer += (str.charAt(i)-'0') * nums;
             nums *= 3;
         }
         return answer;
