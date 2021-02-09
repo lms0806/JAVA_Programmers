5505fbb19c37545e41638f433f545e9194b0c4b4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 23:36:24 +0900

    Update 이상한 문자 만들기.java

--- level1/이상한 문자 만들기.java
+++ level1/이상한 문자 만들기.java
@@ -3,9 +3,10 @@ class Solution {
       String answer = "";
       String[] str = s.split("");
       int count = 0;
-      for(int i = 0; i < str.length; i++){
-          if(str[i].equals(" ")){
-              answer+=" ";
+      
+      for(int i = 0; i < s.length(); i++){
+          if(s.charAt(i) == ' '){
+              answer += " ";
               count = 0;
               continue;
           }
