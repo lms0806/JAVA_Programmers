824194e99bfe0c57d48fd5e6f04987a344f27026
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 23:31:53 +0900

    Update 문자열 다루기 기본.java

--- level1/문자열 다루기 기본.java
+++ level1/문자열 다루기 기본.java
@@ -1,14 +1,10 @@
 class Solution {
-  public boolean solution(String s) {
-      boolean answer = false;
-      String a = s.replaceAll("[^0-9]", "");
-      String[] str = a.split("");
-      String[] str1 = s.split("");
-      
-      if((str1.length == 4 || str1.length == 6) && str.length == str1.length){
-          answer = true;
-      }
-      
-      return answer;
-  }
+    public boolean solution(String s) {
+        boolean answer = false;
+        String a = s.replaceAll("[^0-9]", "");
+        if((s.length() == 4 || s.length() == 6) && a.length() == s.length()){
+            answer = true;
+        }
+        return answer;
+    }
 }
