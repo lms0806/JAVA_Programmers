523e377becbf6bcdb04370475eeb8f9a6cfc4d2b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 22:58:00 +0900

    Update 주식가격.java

--- level2/주식가격.java
+++ level2/주식가격.java
@@ -3,15 +3,14 @@ class Solution {
         int[] answer = new int[prices.length];
         
         for(int i = 0; i < prices.length; i++){
+            int count = 0;
             for(int j = i+1; j < prices.length; j++){
-                if(prices[i] <= prices[j]){
-                    answer[i]++;
-                }
-                else {
-                    answer[i]++;
+                count++;
+                if(prices[i] > prices[j]){
                     break;
                 }
             }
+            answer[i] = count;
         }
         return answer;
     }
