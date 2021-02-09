5a0f139ba68a7570d87e4bcd6a6b19185a20402c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 22:38:45 +0900

    Update [1차]다트 게임.java

--- level1/[1차]다트 게임.java
+++ level1/[1차]다트 게임.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
-        String[] str = dartResult.split("");
-        int[] num = new int[str.length];
+        int[] num = new int[dartResult.length()];
         
-        for(int i = 0; i < str.length; i++){
-            if(str[i].equals("*")){
+        for(int i = 0; i < dartResult.length(); i++){
+            char ch = dartResult.charAt(i);
+            if(ch == '*'){
                 num[i-1] *=2;
                 if(i >= 4){
                     if(num[i-3] != 0){
@@ -16,36 +16,30 @@ class Solution {
                     }
                 }
             }
-            else if(str[i].equals("#")){
+            else if(ch == '#'){
                 num[i-1] = num[i-1]*-1;
             }
-            if(str[i].equals("S")){
-                if(!str[i-1].equals("0")){
-                    num[i] = Integer.parseInt(str[i-1]);
+            if(ch == 'S'){
+                if(i > 0 && dartResult.charAt(i-1) != '0'){
+                    num[i] = dartResult.charAt(i-1) - '0';
                 }
-                else if(i > 1 && str[i-2].equals("1")){
+                else if(i > 1 && dartResult.charAt(i-2) == '1'){
                     num[i] = 10;
                 }
-                else{
-                    num[i] = 0;
-                }
             }
-            else if(str[i].equals("D")){
-                if(!str[i-1].equals("0")){
-                    num[i] = Integer.parseInt(str[i-1])*Integer.parseInt(str[i-1]);
+            else if(ch == 'D'){
+                if(i > 0 && dartResult.charAt(i-1) != '0'){
+                    num[i] = (dartResult.charAt(i-1) - '0')*(dartResult.charAt(i-1) - '0');
                 }
-                else if(i > 1 && str[i-2].equals("1")){
+                else if(i > 1 && dartResult.charAt(i-2) == '1'){
                     num[i] = 100;
                 }
-                else{
-                    num[i] = 0;
-                }
             }
-            else if(str[i].equals("T")){
-                if(!str[i-1].equals("0")){
-                    num[i] = Integer.parseInt(str[i-1])*Integer.parseInt(str[i-1])*Integer.parseInt(str[i-1]);
+            else if(ch == 'T'){
+                if(i > 0 && dartResult.charAt(i-1) != '0'){
+                    num[i] = (dartResult.charAt(i-1) - '0') * (dartResult.charAt(i-1) - '0') * (dartResult.charAt(i-1) - '0');
                 }
-                else if(i > 1 && str[i-2].equals("1")){
+                else if(i > 1 && dartResult.charAt(i-2) == '1'){
                     num[i] = 1000;
                 }
                 else{
@@ -57,9 +51,7 @@ class Solution {
         for(int i = 0; i < num.length; i++){
             if(num[i] != 0){
                 answer+=num[i];
-                //System.out.println(num[i]);
             }
-            //System.out.println(num[i]);
         }
         return answer;
     }
