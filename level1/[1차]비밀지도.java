536d857eb5a2d677492d666a1e475cf73a677fc4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 22:40:44 +0900

    Update [1차]비밀지도.java

--- level1/[1차]비밀지도.java
+++ level1/[1차]비밀지도.java
@@ -16,7 +16,7 @@ class Solution {
                     str[i] = "0" + str[i];
                 }
             }
-            System.out.println(str[i]);
+            
             for(int j = 0; j < n; j++){
                 if(j != n-1){
                     if(str[i].substring(j,j+1).equals("1")){
@@ -27,22 +27,15 @@ class Solution {
                     }
                 }
                 else{
-                    //System.out.println(str[i].substring(j));
                     if(str[i].substring(j).equals("1")){
                         answer[i]+="#";
                     }
                     else{
-                        //System.out.println("i : " + i);
-                        //System.out.println("j : " + j);
                         answer[i]+=" ";
                     }
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            System.out.println(answer[i]);
-        }
-        
         
         return answer;
     }
