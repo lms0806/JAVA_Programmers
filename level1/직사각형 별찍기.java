cb5f8e2755c83c51ee560a3dc23a1d4ae6e1c4dd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 22:44:31 +0900

    Update 직사각형 별찍기.java

--- level1/직사각형 별찍기.java
+++ level1/직사각형 별찍기.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < b; i++){
             for(int j = 0; j < a; j++){
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.print("\n");
+            sb.append("\n");
         }
+        System.out.print(sb);
     }
 }
