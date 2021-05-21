ecf8dd6547b604b9ffc15c1be475be94a26323ef
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-21 18:42:35 +0900

    Update 전화번호 목록.java

--- level2/전화번호 목록.java
+++ level2/전화번호 목록.java
@@ -1,15 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        Arrays.sort(phone_book);
         
-        for(int i = 0; i < phone_book.length; i++){
-            for(int j = i+1; j < phone_book.length; j++){
-                if(phone_book[j].indexOf(phone_book[i]) == 0 || phone_book[i].indexOf(phone_book[j]) == 0){
-                    answer = false;
-                    break;
-                }
+        for(int i = 1; i < phone_book.length; i++){
+            if(phone_book[i].startsWith(phone_book[i-1])){
+                return false;
             }
         }
-        return answer;
+        return true;
     }
 }
