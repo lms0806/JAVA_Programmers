3005cc9049740ff4e4a14e3e06a127607f0a7047
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-24 19:37:05 +0900

    Update 괄호 회전하기.java

--- level2/괄호 회전하기.java
+++ level2/괄호 회전하기.java
@@ -1,60 +1,59 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
         
         for(int i = 0; i < s.length(); i++){
-            int a = 0;
+            int a;
             int count = 0, count1 = 0, count2 = 0;
             ArrayList<Character> arr = new ArrayList<>();
-            for(int j = 0; j < s.length(); j++){
-                char ch = s.charAt(j);
+            for(a = 0; a < s.length(); a++){
+                char ch = s.charAt(a);
                 
-                if(ch == '('){
-                    count++;
+                if(ch == '(' || ch == '{' || ch == '['){
+                    if(ch == '('){
+                        count++;
+                    }
+                    else if(ch == '{'){
+                        count1++;
+                    }
+                     else if(ch == '['){
+                        count2++;
+                    }
                     arr.add(ch);
                 }
-                else if(ch == '{'){
-                    count1++;
-                    arr.add(ch);
-                }
-                else if(ch == '['){
-                    count2++;
-                    arr.add(ch);
-                }
-                
-                if(ch == ')'){
-                    if(count == 0 || (arr.size() != 0 && arr.get(arr.size()-1) != '(')){
+                else{
+                    if(arr.size() == 0){
                         break;
                     }
-                    count--;
-                    arr.remove(arr.size()-1);
-                }
-                else if(ch == '}'){
-                    if(count1 == 0 || (arr.size() != 0 && arr.get(arr.size()-1) != '{')){
-                        break;
+                    if(ch == ')'){
+                        if(count == 0 || arr.get(arr.size()-1) != '('){
+                            break;
+                        }
+                        count--;
                     }
-                    count1--;
-                    arr.remove(arr.size()-1);
-                }
-                else if(ch == ']'){
-                    if(count2 == 0 || (arr.size() != 0 && arr.get(arr.size()-1) != '[')){
-                        break;
+                    else if(ch == '}'){
+                        if(count1 == 0 || arr.get(arr.size()-1) != '{'){
+                            break;
+                        }
+                        count1--;
                     }
-                    count2--;
+                    else if(ch == ']'){
+                        if(count2 == 0 || arr.get(arr.size()-1) != '['){
+                            break;
+                        }
+                        count2--;
+                    }
                     arr.remove(arr.size()-1);
                 }
-                a++;
             }
             
             if(a == s.length() && count == 0 && count1 == 0 && count2 == 0){
                 answer++;
             }
-            s = s.substring(1,s.length()) + s.substring(0,1);
+            s = s.substring(1) + s.substring(0,1);
         }
-        
-        
         return answer;
     }
 }
