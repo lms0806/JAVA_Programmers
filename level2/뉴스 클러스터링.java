0cb3a946a18dc35ca33ad8454ff1d831ff00df0f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-25 03:40:06 +0900

    Create 뉴스 클러스터링.java

---
+++ level2/뉴스 클러스터링.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        double answer = 0;
+        
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        
+        ArrayList<String> arr = new ArrayList<>();
+        ArrayList<String> arr2 = new ArrayList<>();
+        
+        int count = 0, count1 = 0;
+        for(int i = 0; i < str1.length()-1; i++){
+            String s = str1.substring(i,i+2).replaceAll("[^a-z]","");
+            if(s.length() == 2){
+                arr.add(s);
+            }
+        }
+        
+        for(int i = 0; i < str2.length()-1; i++){
+            String s = str2.substring(i,i+2).replaceAll("[^a-z]","");
+            if(s.length() == 2){
+                arr2.add(s);
+            }
+        }
+
+        
+        for(int i = 0; i < arr.size(); i++){
+           if(arr2.remove(arr.get(i))){
+               count++;
+            }
+            count1++;
+        }
+        
+        count1 += arr2.size();
+        answer = (double)count/count1 * 65536;
+        
+        if(count1 == 0){
+            return 65536;
+        }
+        return (int)answer;
+    }
+}
