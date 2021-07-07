114e8c736d6eed22e0a79600b6fa9911457ff834
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-07 22:15:20 +0900

    Update 수박수박수박수박수박수?.java

--- level1/수박수박수박수박수박수?.java
+++ level1/수박수박수박수박수박수?.java
@@ -1,19 +1,5 @@
 class Solution {
   public String solution(int n) {
-      String answer = "";
-      String a = "수";
-      String a1 = "박";
-      int i = 0;
-      
-      while(i != n){
-          if(i%2 == 0){
-              answer+=a;
-          }
-          else{
-              answer+=a1;
-          }
-          i++;
-      }
-      return answer;
+      return "수박".repeat(n).substring(0,n);
   }
 }
