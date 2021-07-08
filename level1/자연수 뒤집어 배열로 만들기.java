b5f357113a737ae6954dc47f8ceec62897f489b9
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 01:29:51 +0900

    Update 자연수 뒤집어 배열로 만들기.java

--- level1/자연수 뒤집어 배열로 만들기.java
+++ level1/자연수 뒤집어 배열로 만들기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] solution(long n) {
-        String[] strs = Long.toString(n).split("");
+        String[] strs = new StringBuilder(Long.toString(n)).reverse().toString().split("");
         int[] answer = new int[strs.length];
         
         for(int i = 0; i < strs.length; i++){
-            answer[strs.length-i-1] = Integer.parseInt(strs[i]); 
+            answer[i] = Integer.parseInt(strs[i]);
         }
         return answer;
     }
