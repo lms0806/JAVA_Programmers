e9e3b22f67bfc771c9b89238158dced346a8a74a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 01:24:49 +0900

    Update 정수 내림차순으로 배치하기.java

--- level1/정수 내림차순으로 배치하기.java
+++ level1/정수 내림차순으로 배치하기.java
@@ -1,24 +1,11 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-  public long solution(long n) {
-      long answer = 0;
-      String str = "";
-      String[] num = new String[(int)Math.log10(n)+1];
-      
-      int a = 0;
-      while(n!=0){
-          num[a] = Long.toString(n%10);
-          n/=10;
-          a++;
-      }
-      
-      Arrays.sort(num);
-      
-      for(int i = num.length-1; i >= 0; i--){
-          str+=num[i];
-      }
-      answer = Long.parseLong(str);
-      return answer;
-  }
+    public long solution(long n) {
+        char[] c = Long.toString(n).toCharArray();
+        
+        Arrays.sort(c);
+        
+        return Long.parseLong(new StringBuilder(new String(c)).reverse().toString());
+    }
 }
