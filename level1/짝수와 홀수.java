c3f1da06b65b8ae0a4b74414aee4e7adec0033e5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-08 23:47:06 +0900

    Update 짝수와 홀수.java

--- level1/짝수와 홀수.java
+++ level1/짝수와 홀수.java
@@ -1,10 +1,5 @@
 class Solution {
   public String solution(int num) {
-      String answer = "Odd";
-      
-      if(num%2 == 0){
-          answer = "Even";
-      }
-      return answer;
+      return num % 2 == 0 ? "Even" : "Odd";
   }
 }
