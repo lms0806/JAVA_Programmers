5dd1fb74d1b52c03afc68633a744c7ea16b98065
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-08 23:18:56 +0900

    Update 평균 구하기.java

--- level1/평균 구하기.java
+++ level1/평균 구하기.java
@@ -1,11 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
   public double solution(int[] arr) {
-      double answer = 0;
-      
-      for(int i = 0; i < arr.length; i++){
-          answer+= arr[i];
-      }
-      answer /= arr.length;
-      return answer;
+      return Arrays.stream(arr).average().orElse(0);
   }
 }
