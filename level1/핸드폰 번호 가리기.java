aa151cf3de24017ffe1a7dddd9541858c774082e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-08 23:14:36 +0900

    Update 핸드폰 번호 가리기.java

--- level1/핸드폰 번호 가리기.java
+++ level1/핸드폰 번호 가리기.java
@@ -1,12 +1,5 @@
 class Solution {
-  public String solution(String phone_number) {
-      String answer = "";
-      String a = phone_number.substring(phone_number.length()-4,phone_number.length());
-      String a1 = phone_number.substring(0,phone_number.length()-4);
-      
-      a1 = a1.replaceAll(".","*");
-      
-      answer= a1+a;
-      return answer;
+  public String solution(String phone_number) {      
+      return phone_number.substring(0,phone_number.length()-4).replaceAll(".","*") + phone_number.substring(phone_number.length()-4);
   }
 }
