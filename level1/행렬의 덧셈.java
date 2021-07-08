baf200391af1869e61a5a527f118ed5ed0d9405f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-08 23:16:21 +0900

    Update 행렬의 덧셈.java

--- level1/행렬의 덧셈.java
+++ level1/행렬의 덧셈.java
@@ -1,10 +1,10 @@
 class Solution {
   public int[][] solution(int[][] arr1, int[][] arr2) {
-      int[][] answer = new int[arr1.length][arr1[0].length];
+      int[][] answer = arr1;
       
       for(int i = 0; i < answer.length; i++){
           for(int j = 0; j < answer[i].length; j++){
-              answer[i][j] = arr1[i][j] + arr2[i][j];
+              answer[i][j] += arr2[i][j];
           }
       }
       return answer;
