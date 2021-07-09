a587e29049ae7bb403ef726f3a2ea1860ba241e1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 12:13:46 +0900

    Update 두 정수 사이의 합.java

--- level1/두 정수 사이의 합.java
+++ level1/두 정수 사이의 합.java
@@ -1,22 +1,9 @@
 class Solution {
   public long solution(int a, int b) {
       long answer = 0;
-      int tmp = 0;
       
-      if(a > b){
-          tmp = a;
-          a = b;
-          b = tmp;
-      }
-      
-      if(a != b){
-          for(int i = a+1; i < b; i++){
-              answer+=i;
-          }
-          answer+=a+b;
-      }
-      else{
-          answer = b;
+      for(int i = Math.min(a,b); i <= Math.max(a,b); i++){
+          answer += i;
       }
       return answer;
   }
