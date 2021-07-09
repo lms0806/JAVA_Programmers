8059ed0eac96dfc51e0e6e73c3a7d8ee1d936e55
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 12:34:23 +0900

    Update [1차]다트 게임.java

--- level1/[1차]다트 게임.java
+++ level1/[1차]다트 게임.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
@@ -6,18 +8,18 @@ class Solution {
         for(int i = 0; i < dartResult.length(); i++){
             char ch = dartResult.charAt(i);
             if(ch == '*'){
-                num[i-1] *=2;
+                num[i - 1] *= 2;
                 if(i >= 4){
-                    if(num[i-3] != 0){
-                        num[i-3] *=2;
+                    if(num[i - 3] != 0){
+                        num[i - 3] *= 2;
                     }
-                    else if(num[i-4] != 0){
-                        num[i-4]*=2;
+                    else if(num[i - 4] != 0){
+                        num[i - 4] *= 2;
                     }
                 }
             }
             else if(ch == '#'){
-                num[i-1] = num[i-1]*-1;
+                num[i - 1] *= -1;
             }
             if(ch == 'S'){
                 if(i > 0 && dartResult.charAt(i-1) != '0'){
@@ -29,15 +31,15 @@ class Solution {
             }
             else if(ch == 'D'){
                 if(i > 0 && dartResult.charAt(i-1) != '0'){
-                    num[i] = (dartResult.charAt(i-1) - '0')*(dartResult.charAt(i-1) - '0');
+                    num[i] = (int)Math.pow((dartResult.charAt(i - 1) - '0'), 2);
                 }
-                else if(i > 1 && dartResult.charAt(i-2) == '1'){
+                else if(i > 1 && dartResult.charAt(i - 2) == '1'){
                     num[i] = 100;
                 }
             }
             else if(ch == 'T'){
                 if(i > 0 && dartResult.charAt(i-1) != '0'){
-                    num[i] = (dartResult.charAt(i-1) - '0') * (dartResult.charAt(i-1) - '0') * (dartResult.charAt(i-1) - '0');
+                    num[i] = (int)Math.pow((dartResult.charAt(i - 1) - '0'), 3);
                 }
                 else if(i > 1 && dartResult.charAt(i-2) == '1'){
                     num[i] = 1000;
@@ -47,12 +49,6 @@ class Solution {
                 }
             }
         }
-        
-        for(int i = 0; i < num.length; i++){
-            if(num[i] != 0){
-                answer+=num[i];
-            }
-        }
-        return answer;
+        return IntStream.of(num).sum();
     }
 }
