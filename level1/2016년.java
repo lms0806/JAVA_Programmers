112e1c60907a459e6f9504f4ba8f7990d77203fb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 13:21:32 +0900

    Update 2016년.java

--- level1/2016년.java
+++ level1/2016년.java
@@ -1,28 +1,13 @@
 class Solution {
   public String solution(int a, int b) {
-      String answer = "";
+      int[] num = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
       
       for(int i = 1; i < a; i++){
-          if(i%2 == 1 && i < 8){
-              b+=31;
-          }
-          else if(i%2 == 0 && i < 8 && i != 2){
-              b+=30;
-          }
-          else if(i == 2){
-              b+=29;
-          }
-          else if(i%2 == 1 && i >= 8){
-              b+=30;
-          }
-          else if(i%2 == 0 && i >= 8){
-              b+=31;
-          }
+          b += num[i-1];
       }
       
       String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-      answer = day[(b-1)%7];
       
-      return answer;
+      return day[(b-1)%7];
   }
 }
