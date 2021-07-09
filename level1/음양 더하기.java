7bd35f7cfe4e51d159a56d66385ea75bc412b5af
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 16:05:29 +0900

    Update 음양 더하기.java

--- level1/음양 더하기.java
+++ level1/음양 더하기.java
@@ -3,13 +3,9 @@ class Solution {
         int answer = 0;
         
         for(int i = 0; i < signs.length; i++){
-            if(signs[i]){
-                answer += absolutes[i];
-            }
-            else{
-                answer -= absolutes[i];
-            }
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
         }
+        
         return answer;
     }
 }
