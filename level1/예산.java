2aef5c1fdc4865af5d5688ad649413b3d977212c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 13:23:52 +0900

    Update 예산.java

--- level1/예산.java
+++ level1/예산.java
@@ -1,17 +1,19 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
   public int solution(int[] d, int budget) {
       int answer = 0;
-      int num = 0;
       
       Arrays.sort(d);
       
       for(int i = 0; i < d.length; i++){
-          if(num+d[i] <= budget){
-              num+=d[i];
-              answer++;
+          budget -= d[i];
+          
+          if(budget < 0){
+              break;
           }
+          
+          answer++;
       }
       return answer;
   }
