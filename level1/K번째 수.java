37bf3a4aab05fb4f1c129e9ac7734f123c79346a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 16:02:39 +0900

    Update K번째 수.java

--- level1/K번째 수.java
+++ level1/K번째 수.java
@@ -6,14 +6,12 @@ class Solution {
         int[] split;
 
         for(int i = 0; i < commands.length; i++){
-            int idx = 0;
             split = new int[commands[i][1] - commands[i][0] + 1];
             for(int j = commands[i][0]-1; j <= commands[i][1]-1; j++){
-                split[idx] = array[j];
-                idx++;
+                split[j - commands[i][0] + 1] = array[j];
             }
             Arrays.sort(split);
-            answer[i] = split[commands[i][2]-1];
+            answer[i] = split[commands[i][2] - 1];
         }
         
         return answer;
