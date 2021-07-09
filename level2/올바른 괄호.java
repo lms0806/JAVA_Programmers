453acaa7d6b94d16967a67a24093f4dfe2fb4081
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 16:21:59 +0900

    Update 올바른 괄호.java

--- level2/올바른 괄호.java
+++ level2/올바른 괄호.java
@@ -1,10 +1,9 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
         int count = 0;
         
         if(s.charAt(0) == ')' || s.charAt(s.length()-1) == '('){
-            answer = false;
+            return false;
         }
         else{
             for(int i = 0; i < s.length(); i++){
@@ -16,12 +15,11 @@ class Solution {
                     count--;
                 }
                 if(count >= s.length()-i){
-                    answer = false;
-                    break;
+                    return false;
                 }
             }
         }
 
-        return answer;
+        return true;
     }
 }
