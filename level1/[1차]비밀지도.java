810cec35ff1f7f3f390a4e7d92e278c4f775a392
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 12:41:53 +0900

    Update [1차]비밀지도.java

--- level1/[1차]비밀지도.java
+++ level1/[1차]비밀지도.java
@@ -1,28 +1,11 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String[] str = new String[n];
         
         for(int i = 0; i < n; i++){
-            str[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            answer[i] = "";
-        }
-        
-        for(int i = 0; i < n; i++){
-            if(str[i].length() < n){
-                while(str[i].length() != n){
-                    str[i] = "0" + str[i];
-                }
-            }
-            
-            for(int j = 0; j < n; j++){
-                if(j != n-1){
-                    answer[i] += str[i].substring(j, j+1).equals("1") ? "#" : " ";
-                }
-                else{
-                    answer[i] += str[i].substring(j).equals("1") ? "#" : " ";
-                }
-            }
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = String.format("%" + n + "s", answer[i]);
+            answer[i] = answer[i].replaceAll("1","#").replaceAll("0"," ");
         }
         
         return answer;
