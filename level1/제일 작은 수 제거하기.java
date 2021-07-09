965fb33a2c24aaa07181a6aa1c5ef82c81d84ad2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 23:09:56 +0900

    Update 제일 작은 수 제거하기.java

--- level1/제일 작은 수 제거하기.java
+++ level1/제일 작은 수 제거하기.java
@@ -2,12 +2,12 @@ class Solution {
     public int[] solution(int[] arr) {
         int[] answer = new int[arr.length == 1 ? 1 : arr.length - 1];
         
-        int num = arr[0];
         if(answer.length == 1){
             answer[0] = -1;
             return answer;
         }
         else{
+            int num = arr[0];
             for(int n : arr){
                 if(n < num){
                     num = n;
