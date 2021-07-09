950be517e38ed3480e83cfcf800e56f6f2686dde
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 11:45:32 +0900

    Update 문자열 정수로 바꾸기.java

--- level1/문자열 정수로 바꾸기.java
+++ level1/문자열 정수로 바꾸기.java
@@ -1,8 +1,5 @@
 class Solution {
   public int solution(String s) {
-      int answer = 0;
-      
-      answer =  Integer.parseInt(s);
-      return answer;
+      return Integer.parseInt(s);
   }
 }
