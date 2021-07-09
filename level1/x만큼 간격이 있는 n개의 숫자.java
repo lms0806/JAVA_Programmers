42a0ac1a7181aa90870dcf7fa4ae3c3325b4e797
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 22:49:21 +0900

    Update x만큼 간격이 있는 n개의 숫자.java

--- level1/x만큼 간격이 있는 n개의 숫자.java
+++ level1/x만큼 간격이 있는 n개의 숫자.java
@@ -2,9 +2,8 @@ class Solution {
   public long[] solution(int x, int n) {
       long[] answer = new long[n];
       
-      answer[0] = x;
-      for(int i = 1; i < n; i++){
-          answer[i] = x+answer[i-1];
+      for(int i = 1; i <= n; i++){
+          answer[i-1] = (long)x * i;
       }
       return answer;
   }
