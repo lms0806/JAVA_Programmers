a09d5cf698a49c3472fbd671d08d08fa47735a54
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 16:08:51 +0900

    Update 체육복.java

--- level1/체육복.java
+++ level1/체육복.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
         int count = 0;
+        
         for(int i = 0; i < lost.length; i++){
             for(int j = 0; j < reserve.length; j++){
                 if(lost[i] == reserve[j]){
@@ -29,7 +29,6 @@ class Solution {
             }
         }
         
-        answer = n - count;
-        return answer;
+        return n - count;
     }
 }
