0a038d3448b23c42d6ccf6dea3e2479073c80373
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 11:59:21 +0900

    Update 서울에서 김서방 찾기.java

--- level1/서울에서 김서방 찾기.java
+++ level1/서울에서 김서방 찾기.java
@@ -1,13 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
   public String solution(String[] seoul) {
-      String answer = "";
-      
-      for(int i = 0; i < seoul.length; i++){
-          if(seoul[i].equals("Kim")){
-              answer="김서방은 " + i + "에 있다";
-              break;
-          }
-      }
-      return answer;
+      return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
   }
 }
