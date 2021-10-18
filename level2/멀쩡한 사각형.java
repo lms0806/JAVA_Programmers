106d7c5d22cf3ed7f8b6002895e2ad48c8520701
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-18 12:56:47 +0900

    Update 멀쩡한 사각형.java

--- level2/멀쩡한 사각형.java
+++ level2/멀쩡한 사각형.java
@@ -1,10 +1,6 @@
 class Solution {
     public long solution(long w, long h) {
-        long answer;
-    
-        answer = (w * h) - (w + h - gcd(w,h));
-        
-        return answer;
+        return (w * h) - (w + h - gcd(w,h));
     }
     public long gcd(long w, long h){
         return (w == 0) ? h : gcd((h%w), w);
