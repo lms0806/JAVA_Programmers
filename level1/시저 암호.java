052a1d28fee8e088d5e3e30f3e65507f43cb2212
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-21 23:56:52 +0900

    Update 시저 암호.java

--- level1/시저 암호.java
+++ level1/시저 암호.java
@@ -1,30 +1,22 @@
 class Solution {
-  public String solution(String s, int n) {
-      String answer = "";
-      
-      byte[] bte = new byte[s.length()];
-      
-      for(int i = 0; i < s.length(); i++){
-          bte[i] = (byte)s.charAt(i);
-          if(bte[i] != 32){
-              for(int j = 0; j < n; j++){
-                  bte[i]++;
-                  if(bte[i] == 123){
-                      bte[i] = 97;
-                  }//소문자
-                  if(bte[i] == 91){
-                      bte[i] = 65;
-                  }//대문자
-              }
-          }
-          if(bte[i] == 123){
-              bte[i] = 97;
-          }
-      }
-      
-      for(int i = 0; i < s.length(); i++){
-          answer += (char)bte[i];
-      }
-      return answer;
-  }
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        for(char ch : s.toCharArray()){
+            if(ch != ' '){
+                char c = (char)(ch + n);
+                if(ch >= 'a' && ch <= 'z' && c > 'z'){
+                    c -= 26;
+                }
+                else if(ch >= 'A' && ch <= 'Z' && c > 'Z'){
+                    c -= 26;
+                }
+                answer += c;
+            }
+            else{
+                answer += " ";
+            }
+        }
+        return answer;
+    }
 }
