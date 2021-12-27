c868a371b901112ca6c649fc1a7200adaf72635b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-27 23:53:21 +0900

    Create 멀리 뛰기.java

---
+++ level3/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int solution(int n) {
+		int[] dp = new int[n + 1];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+		}
+		
+		return dp[n];
+    }
+}
