b7da5d0850fc550b16ada1ca2c5f7ad7d0a6ee6b
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-10 20:34:12 +0900

    Update 폰켓몬.java

--- level2/폰켓몬.java
+++ level2/폰켓몬.java
@@ -1,25 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
+        boolean[] arr = new boolean[200001];
         
-        Arrays.sort(nums);
-        
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] == nums[i-1]){
-                nums[i-1] = 0;
-            }
-        }
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
+        for(int n : nums){
+            if(!arr[n]){
+                arr[n] = true;
                 answer++;
             }
-            if(answer > nums.length/2){
-                answer = nums.length/2;
-            }
         }
-        return answer;
+        
+        return Math.min(answer, nums.length / 2);
     }
 }
