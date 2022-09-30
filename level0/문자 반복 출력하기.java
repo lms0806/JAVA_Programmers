7002b5e88953c3dd053d4d83da9bf7b6810af14b
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-09-30 14:07:15 +0900

    Create 문자 반복 출력하기.java

---
+++ level0/문자 반복 출력하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            sb.append((c + "").repeat(n));
+        }
+        return sb.toString();
+    }
+}
