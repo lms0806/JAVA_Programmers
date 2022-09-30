652912f0209c767dc5b4f0eeac59e2a504d0c08c
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-09-30 14:08:04 +0900

    Create 짝수 홀수 개수.java

---
+++ level0/짝수 홀수 개수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        for(int i = 0; i < num_list.length; i++){
+            answer[num_list[i] % 2]++;
+        }
+        return answer;
+    }
+}
