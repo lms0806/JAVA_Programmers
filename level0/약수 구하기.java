1dbebc0b96ff842155d6bff3986f1402b0c8ced9
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-01 15:59:09 +0900

    Create 약수 구하기.java

---
+++ level0/약수 구하기.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                arr.add(i);
+            }
+        }
+        
+        int[] answer = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
