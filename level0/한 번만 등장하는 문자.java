24aa4068aa0e061f638733caec82e721ca7be47c
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-03 00:42:27 +0900

    Create 한 번만 등장하는 문자.java

---
+++ level0/한 번만 등장하는 문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        int[] alpha = new int[26];
+        for(char c : s.toCharArray()){
+            alpha[c - 'a']++;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] == 1){
+                answer.append((char)(i + 'a'));
+            }
+        }
+        return answer.toString();
+    }
+}
