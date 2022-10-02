d42ddb259bebcece1b2420a4a4bb2c75bc1bf2d0
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-03 01:30:22 +0900

    Update 옷가게 할인 받기.java

--- level0/옷가게 할인 받기.java
+++ level0/옷가게 할인 받기.java
@@ -3,13 +3,13 @@ class Solution {
         int answer = 0;
         
         if(price >= 500000){
-            price -= price / 5;
+            price *= 0.8;
         }
         else if(price >= 300000){
-            price -= price / 10;
+            price *= 0.9;
         }
         else if(price >= 100000){
-            price -= price / 20;
+            price *= 0.95;
         }
         return price;
     }
