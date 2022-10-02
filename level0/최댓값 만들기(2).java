8578a9807e4aeddedf51dcdcb0bc7f1a9ee5661f
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-02 19:29:02 +0900

    Update 최댓값 만들기(2).java

--- level0/최댓값 만들기(2).java
+++ level0/최댓값 만들기(2).java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
+        int answer = Integer.MIN_VALUE;
         for(int i = 0; i < numbers.length; i++){
             for(int j = i + 1; j < numbers.length; j++){
                 answer = Math.max(answer, numbers[i] * numbers[j]);
