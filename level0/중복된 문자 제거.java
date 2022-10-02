efee61183462bff34c8b80a301505794a847be13
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-02 19:27:09 +0900

    Update 중복된 문자 제거.java

--- level0/중복된 문자 제거.java
+++ level0/중복된 문자 제거.java
@@ -1,30 +1,13 @@
 class Solution {
     public String solution(String my_string) {
-        int[] alpha = new int[26], alpha2 = new int[26];
+        boolean[] check = new boolean[256];
         
-        int count = 0;
         StringBuilder answer = new StringBuilder();
         for(char c : my_string.toCharArray()){
-            if(c >= 'a' && c <= 'z'){
-                alpha[c - 'a']++;
-                
-                if(alpha[c - 'a'] == 1){
-                    answer.append(c);
-                }
+            if(!check[c]){
+                answer.append(c);
             }
-            else if(c >= 'A' && c <= 'Z'){
-                alpha2[c - 'A']++;
-                
-                if(alpha2[c - 'A'] == 1){
-                    answer.append(c);
-                }
-            }
-            else{
-                count++;
-                if(count == 1){
-                    answer.append(c);
-                }
-            }
+            check[c] = true;
         }
         
         return answer.toString();
