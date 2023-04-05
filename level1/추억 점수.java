87de1d893af4db9c719b39cbe92bf84acf240f6a
lms0806 <godmlzkf1@naver.com>
2023-04-05 18:11:37 +0900

    Create 추억 점수.java

---
+++ level1/추억 점수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for(int i = 0; i < photo.length; i++){
+            for(int j = 0; j < photo[i].length; j++){
+                for(int k = 0; k < name.length; k++){
+                    if(name[k].equals(photo[i][j])){
+                        answer[i] += yearning[k];
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
