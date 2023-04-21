0a66b2012a9a63a7b3ef1e04cfc62e100e92bdac
lms0806 <godmlzkf1@naver.com>
2023-04-21 14:43:49 +0900

    Create 길이에 따른 연산.java

---
+++ level0/길이에 따른 연산.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        if(num_list.length > 10){
+            for(int n : num_list){
+                answer += n;
+            }
+        }
+        else{
+            answer = 1;
+            for(int n : num_list){
+                answer *= n;
+            }
+        }
+        return answer;
+    }
+}
