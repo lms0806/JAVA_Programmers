15a0092f7a513af08d14f52655ce08a2ea2e5c4f
lms0806 <godmlzkf1@naver.com>
2023-04-21 14:46:55 +0900

    Update 문자 리스트를 문자열로 변환하기.java

--- level0/문자 리스트를 문자열로 변환하기.java
+++ level0/문자 리스트를 문자열로 변환하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String[] arr) {
-        String answer = "";
-        for(String s : arr){
-            answer += s;
-        }
-        return answer;
+        return String.join("", arr);
     }
 }
