c6cd72dd02b39134b98e59cd21f5295db7986267
lms0806 <godmlzkf1@naver.com>
2023-04-21 15:02:23 +0900

    Create 문자열 정수의 합.java

---
+++ level0/문자열 정수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String num_str) {
+        int answer = 0;
+        
+        for(char c : num_str.toCharArray()){
+            answer += c - '0';
+        }
+        
+        return answer;
+    }
+}
