ce806916c0d73e86f222d89e855d2309fce5eb12
lms0806 <godmlzkf1@naver.com>
2023-04-22 16:44:52 +0900

    Create 문자열 뒤집기 .java

---
+++ level0/문자열 뒤집기 .java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(my_string.substring(s, e + 1));
+        sb.reverse();
+        
+        return my_string.substring(0, s) + sb.toString() + my_string.substring(e + 1);
+    }
+}
