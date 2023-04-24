b814ba6017b4a876ce1d2c1098f6930e7dadbfe5
lms0806 <godmlzkf1@naver.com>
2023-04-24 18:51:41 +0900

    Create 특별한 이차원 배열 1.java

---
+++ level0/특별한 이차원 배열 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        
+        for(int i = 0; i < n; i++){
+            answer[i][i] = 1;
+        }
+        
+        return answer;
+    }
+}
