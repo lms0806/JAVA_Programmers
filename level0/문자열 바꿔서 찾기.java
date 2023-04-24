27f810b249a31574413ac353c1d69c7518e2d278
lms0806 <godmlzkf1@naver.com>
2023-04-24 21:52:07 +0900

    Create 문자열 바꿔서 찾기.java

---
+++ level0/문자열 바꿔서 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : myString.toCharArray()){
+            sb.append(c == 'A' ? 'B' : 'A');
+        }
+        
+        return sb.toString().contains(pat) ? 1 : 0;
+    }
+}
