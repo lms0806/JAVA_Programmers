3d2c2e2d4b4df57629f9077410a43ac5e0edca03
lms0806 <godmlzkf1@naver.com>
2023-04-26 13:26:18 +0900

    Create 수열과 구간 쿼리 2.java

---
+++ level0/수열과 구간 쿼리 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            answer[i] = 1000001;
+            
+            for(int j = queries[i][0]; j <= queries[i][1]; j++){
+                if(arr[j] > queries[i][2]){
+                    answer[i] = Math.min(answer[i], arr[j]);
+                }
+            }
+            
+            if(answer[i] == 1000001){
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
