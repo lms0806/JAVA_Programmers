775a8b8da0b6384f1deb5ff1062dbf787383a9e5
lms0806 <godmlzkf1@naver.com>
2023-04-26 13:21:07 +0900

    Create 배열의 길이를 2의 거듭제곱으로 만들기.java

---
+++ level0/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int size = 2;
+        while(size < arr.length){
+            size *= 2;
+        }
+        
+        int[] answer = new int[size];
+        for(int i = 0; i < arr.length; i++){
+            answer[i] = arr[i];
+        }
+        return answer;
+    }
+}
